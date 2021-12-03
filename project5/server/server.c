03fb713a12dedeae1c530bfd5956efbe80a89f89
Chase <chase.anderson.000@gmail.com>
2021-12-03 00:27:12 -0600

    beginMultithread

--- project5/server/server.c
+++ project5/server/server.c
@@ -5,12 +5,47 @@
 #include <pthread.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
 pthread_mutex_t leterLock = PTHREAD_MUTEX_INITIALIZER;
+
+int globalArray[26] = {0};
+//int globalArray[26] = { [0 ... 25] = 0 };
+
+int midCtoS;
+int midStoC;
 
 void* process_client(void* param) {
 
+	// REMEMBER TO FREE clientNum
+
+	struct msgbuf recvMsg;
+
+	int thread_num = *((int*)param);
+
+	printf("testing with thread_num: %d\n", thread_num);
+
+	// -----------------
+
+	while (1) {
 
 
+		msgrcv(midCtoS, (void*)&recvMsg, sizeof(recvMsg), thread_num, 0);
+
+		perror("LOL");
+
+		printf("Received: %s", buf.mtext);
+
+	}
+
+	// -----------------
+
+
+	free(param);
+
+	return 0x0;
 
 }
 
@@ -18,30 +53,47 @@ void* process_client(void* param) {
 int main(int argc, char* argv[])
 {
 
-	int threads;
+	for (int i = 0; i < 26; i++) {
 
-	int* clientNum = (int*) malloc(sizeof(threads);
+		printf("%d %d\n", globalArray[i],i);
+
+	}
+
+	int threads;
 
 	if (argc != 2) {
 		printf("Incorrect arg count. Enter thread count\n");
 		exit(EXIT_FAILURE);
 	}
 
-	threads = atoi(argv[2]);
+	threads = atoi(argv[1]);
 
 	if (threads < 1 || threads > 16) {
 		printf("Incorrect number of threads. Enter between 1 and 16\n");
 		exit(EXIT_FAILURE);
 	}
 
+	key_t keyCtoS = 101;
 
-	int i;
+	midCtoS = msgget(keyCtoS, 0666 | IPC_CREAT);
+
+	key_t keyStoC = 808;
+
+	midStoC = msgget(keyStoC, 0666 | IPC_CREAT);
 
 	pthread_t threadArray[threads];
 
+	int i;
+
 	for (i = 0; i < threads; i++)
 	{
+		
+		int* clientNum = (int*)malloc(sizeof(*clientNum));
+
+		*clientNum = i;
+
 		pthread_create(&threadArray[i], NULL, process_client, (void*)clientNum);
+
 	}
 
 	for (i = 0; i < threads; i++)
@@ -49,9 +101,7 @@ int main(int argc, char* argv[])
 		pthread_join(threadArray[i], NULL);
 	}
 
-
 	printf("\n");
 
-	free(filename);
 	return 0;
 }
--- project5/server/server.h
+++ project5/server/server.h
@@ -1,4 +1,11 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define MSGSZ 128
+
+struct msgbuf {
+	long mtype;
+	char mtext[MSGSZ];
+};
+
 #endif
